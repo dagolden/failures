9ece9d841eff71b7da1e91c6a9af17871894a626
David Golden <dagolden@cpan.org>
2013-09-17 14:49:05 -0400

    import

---
+++ lib/failures.pm
@@ -0,0 +1,34 @@
+use v5.10;
+use strict;
+use warnings;
+
+package failures;
+# ABSTRACT: No abstract given for failures
+# VERSION
+
+
+1;
+
+=for Pod::Coverage BUILD
+
+=head1 SYNOPSIS
+
+    use failures;
+
+=head1 DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+=head1 USAGE
+
+Good luck!
+
+=head1 SEE ALSO
+
+=for :list
+* Maybe other modules do related things.
+
+=cut
+
+# vim: ts=4 sts=4 sw=4 et:
